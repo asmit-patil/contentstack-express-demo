6920adbf01aa44201b76ff311e86adaa3ac2af19
asmit-patil <patilasmit0@gmail.com>
2018-06-20 11:22:23 +0530

    initial commit

---
+++ config/all.js
@@ -0,0 +1,15 @@
+module.exports = exports = {
+    port: "4000",
+    theme: "basic",
+    languages: [
+        {
+            "code": "en-us",
+            "relative_url_prefix": "/"
+        }
+    ],
+    plugins: {},
+    contentstack: {
+        api_key: "bltd1343376dfba54d2",
+        access_token: "bltfe57b09b1e4c5732"
+    }
+};
